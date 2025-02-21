3b76bc209ec00e70b6c28e7369fd34002230d3e3
Stefan Abi-Karam <stefanabikaram@gmail.com>
2025-02-21 05:54:02 -0500

    added init machsuite

---
+++ hls_eval_data/machsuite/aes_aes/support.cpp
@@ -0,0 +1,149 @@
+#line 1 "hls-machsuite/benchmarks/aes_aes/support.cpp"
+#include "support.h"
+#include <stdio.h>
+#include <stdarg.h>
+#include <string.h>
+#include <unistd.h>
+#include <assert.h>
+#include <errno.h>
+#include <sys/types.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+
+
+
+
+static inline int fd_printf(int fd, const char *format, ...) {
+  va_list args;
+  int buffered, written, status;
+  char buffer[256];
+  va_start(args, format);
+  buffered = vsnprintf(buffer, 256, format, args);
+  va_end(args);
+  assert(buffered<256 && "Overran fd_printf buffer---output possibly corrupt");
+  written = 0;
+  while(written<buffered) {
+    status = write(fd, &buffer[written], buffered-written);
+    assert(status>=0 && "Write failed");
+    written += status;
+  }
+  assert(written==buffered && "Wrote more data than given");
+  return written;
+}
+
+
+char *readfile(int fd) {
+  char *p;
+  struct stat s;
+  off_t len;
+  ssize_t bytes_read, status;
+
+  assert(fd>1 && "Invalid file descriptor");
+  assert(0==fstat(fd, &s) && "Couldn't determine file size");
+  len = s.st_size;
+  assert(len>0 && "File is empty");
+  p = (char *)malloc(len+1);
+  bytes_read = 0;
+  while( bytes_read<len ) {
+    status = read(fd, &p[bytes_read], len-bytes_read);
+    assert(status>=0 && "read() failed");
+    bytes_read+=status;
+  }
+  p[len] = (char)0;
+  close(fd);
+  return p;
+}
+
+char *find_section_start(char *s, int n) {
+  int i=0;
+
+  assert(n>=0 && "Invalid section number");
+  if(n==0)
+    return s;
+
+
+  while(i<n && (*s)!=(char)0) {
+
+    if( s[0]=='%' && s[1]=='%' && s[2]=='\n' ) {
+      i++;
+    }
+    s++;
+  }
+  if(*s!=(char)0)
+    return s+2;
+  return s;
+}
+
+
+int parse_string(char *s, char *arr, int n) {
+  int k;
+  assert(s!=NULL && "Invalid input string");
+
+  if( n<0 ) {
+    k = 0;
+    while( s[k]!=(char)0 && s[k+1]!=(char)0 && s[k+2]!=(char)0
+        && !(s[k]=='\n'  && s[k+1]=='%'     && s[k+2]=='%') ) {
+      k++;
+    }
+  } else {
+    k = n;
+  }
+
+  memcpy( arr, s, k );
+  if( n<0 )
+    arr[k] = 0;
+
+  return 0;
+}
+#line 132 "hls-machsuite/benchmarks/aes_aes/support.cpp"
+int parse_uint8_t_array(char *s, uint8_t *arr, int n) { char *line, *endptr; int i=0; uint8_t v; assert(s!=NULL && "Invalid input string"); line = strtok(s,"\n"); while( line!=NULL && i<n ) { endptr = line; v = (uint8_t)(strtol(line,&endptr,10)); if( (*endptr)!=(char)0 ) { fprintf(stderr, "Invalid input: line %d of section\n", i); } arr[i] = v; i++; line[strlen(line)] = '\n'; line = strtok(NULL,"\n"); } if(line!=NULL) { line[strlen(line)] = '\n'; } return 0; }
+int parse_uint16_t_array(char *s, uint16_t *arr, int n) { char *line, *endptr; int i=0; uint16_t v; assert(s!=NULL && "Invalid input string"); line = strtok(s,"\n"); while( line!=NULL && i<n ) { endptr = line; v = (uint16_t)(strtol(line,&endptr,10)); if( (*endptr)!=(char)0 ) { fprintf(stderr, "Invalid input: line %d of section\n", i); } arr[i] = v; i++; line[strlen(line)] = '\n'; line = strtok(NULL,"\n"); } if(line!=NULL) { line[strlen(line)] = '\n'; } return 0; }
+int parse_uint32_t_array(char *s, uint32_t *arr, int n) { char *line, *endptr; int i=0; uint32_t v; assert(s!=NULL && "Invalid input string"); line = strtok(s,"\n"); while( line!=NULL && i<n ) { endptr = line; v = (uint32_t)(strtol(line,&endptr,10)); if( (*endptr)!=(char)0 ) { fprintf(stderr, "Invalid input: line %d of section\n", i); } arr[i] = v; i++; line[strlen(line)] = '\n'; line = strtok(NULL,"\n"); } if(line!=NULL) { line[strlen(line)] = '\n'; } return 0; }
+int parse_uint64_t_array(char *s, uint64_t *arr, int n) { char *line, *endptr; int i=0; uint64_t v; assert(s!=NULL && "Invalid input string"); line = strtok(s,"\n"); while( line!=NULL && i<n ) { endptr = line; v = (uint64_t)(strtol(line,&endptr,10)); if( (*endptr)!=(char)0 ) { fprintf(stderr, "Invalid input: line %d of section\n", i); } arr[i] = v; i++; line[strlen(line)] = '\n'; line = strtok(NULL,"\n"); } if(line!=NULL) { line[strlen(line)] = '\n'; } return 0; }
+int parse_int8_t_array(char *s, int8_t *arr, int n) { char *line, *endptr; int i=0; int8_t v; assert(s!=NULL && "Invalid input string"); line = strtok(s,"\n"); while( line!=NULL && i<n ) { endptr = line; v = (int8_t)(strtol(line,&endptr,10)); if( (*endptr)!=(char)0 ) { fprintf(stderr, "Invalid input: line %d of section\n", i); } arr[i] = v; i++; line[strlen(line)] = '\n'; line = strtok(NULL,"\n"); } if(line!=NULL) { line[strlen(line)] = '\n'; } return 0; }
+int parse_int16_t_array(char *s, int16_t *arr, int n) { char *line, *endptr; int i=0; int16_t v; assert(s!=NULL && "Invalid input string"); line = strtok(s,"\n"); while( line!=NULL && i<n ) { endptr = line; v = (int16_t)(strtol(line,&endptr,10)); if( (*endptr)!=(char)0 ) { fprintf(stderr, "Invalid input: line %d of section\n", i); } arr[i] = v; i++; line[strlen(line)] = '\n'; line = strtok(NULL,"\n"); } if(line!=NULL) { line[strlen(line)] = '\n'; } return 0; }
+int parse_int32_t_array(char *s, int32_t *arr, int n) { char *line, *endptr; int i=0; int32_t v; assert(s!=NULL && "Invalid input string"); line = strtok(s,"\n"); while( line!=NULL && i<n ) { endptr = line; v = (int32_t)(strtol(line,&endptr,10)); if( (*endptr)!=(char)0 ) { fprintf(stderr, "Invalid input: line %d of section\n", i); } arr[i] = v; i++; line[strlen(line)] = '\n'; line = strtok(NULL,"\n"); } if(line!=NULL) { line[strlen(line)] = '\n'; } return 0; }
+int parse_int64_t_array(char *s, int64_t *arr, int n) { char *line, *endptr; int i=0; int64_t v; assert(s!=NULL && "Invalid input string"); line = strtok(s,"\n"); while( line!=NULL && i<n ) { endptr = line; v = (int64_t)(strtol(line,&endptr,10)); if( (*endptr)!=(char)0 ) { fprintf(stderr, "Invalid input: line %d of section\n", i); } arr[i] = v; i++; line[strlen(line)] = '\n'; line = strtok(NULL,"\n"); } if(line!=NULL) { line[strlen(line)] = '\n'; } return 0; }
+
+int parse_float_array(char *s, float *arr, int n) { char *line, *endptr; int i=0; float v; assert(s!=NULL && "Invalid input string"); line = strtok(s,"\n"); while( line!=NULL && i<n ) { endptr = line; v = (float)(strtof(line, &endptr)); if( (*endptr)!=(char)0 ) { fprintf(stderr, "Invalid input: line %d of section\n", i); } arr[i] = v; i++; line[strlen(line)] = '\n'; line = strtok(NULL,"\n"); } if(line!=NULL) { line[strlen(line)] = '\n'; } return 0; }
+int parse_double_array(char *s, double *arr, int n) { char *line, *endptr; int i=0; double v; assert(s!=NULL && "Invalid input string"); line = strtok(s,"\n"); while( line!=NULL && i<n ) { endptr = line; v = (double)(strtod(line, &endptr)); if( (*endptr)!=(char)0 ) { fprintf(stderr, "Invalid input: line %d of section\n", i); } arr[i] = v; i++; line[strlen(line)] = '\n'; line = strtok(NULL,"\n"); } if(line!=NULL) { line[strlen(line)] = '\n'; } return 0; }
+
+
+int write_string(int fd, char *arr, int n) {
+  int status, written;
+  assert(fd>1 && "Invalid file descriptor");
+  if( n<0 ) {
+    n = strlen(arr);
+  }
+  written = 0;
+  while(written<n) {
+    status = write(fd, &arr[written], n-written);
+    assert(status>=0 && "Write failed");
+    written += status;
+  }
+
+  do {
+    status = write(fd, "\n", 1);
+    assert(status>=0 && "Write failed");
+  } while(status==0);
+
+  return 0;
+}
+#line 177 "hls-machsuite/benchmarks/aes_aes/support.cpp"
+int write_uint8_t_array(int fd, uint8_t *arr, int n) { int i; assert(fd>1 && "Invalid file descriptor"); for( i=0; i<n; i++ ) { fd_printf(fd, "%" PRIu8 "\n", arr[i]); } return 0; }
+int write_uint16_t_array(int fd, uint16_t *arr, int n) { int i; assert(fd>1 && "Invalid file descriptor"); for( i=0; i<n; i++ ) { fd_printf(fd, "%" PRIu16 "\n", arr[i]); } return 0; }
+int write_uint32_t_array(int fd, uint32_t *arr, int n) { int i; assert(fd>1 && "Invalid file descriptor"); for( i=0; i<n; i++ ) { fd_printf(fd, "%" PRIu32 "\n", arr[i]); } return 0; }
+int write_uint64_t_array(int fd, uint64_t *arr, int n) { int i; assert(fd>1 && "Invalid file descriptor"); for( i=0; i<n; i++ ) { fd_printf(fd, "%" PRIu64 "\n", arr[i]); } return 0; }
+int write_int8_t_array(int fd, int8_t *arr, int n) { int i; assert(fd>1 && "Invalid file descriptor"); for( i=0; i<n; i++ ) { fd_printf(fd, "%" PRId8 "\n", arr[i]); } return 0; }
+int write_int16_t_array(int fd, int16_t *arr, int n) { int i; assert(fd>1 && "Invalid file descriptor"); for( i=0; i<n; i++ ) { fd_printf(fd, "%" PRId16 "\n", arr[i]); } return 0; }
+int write_int32_t_array(int fd, int32_t *arr, int n) { int i; assert(fd>1 && "Invalid file descriptor"); for( i=0; i<n; i++ ) { fd_printf(fd, "%" PRId32 "\n", arr[i]); } return 0; }
+int write_int64_t_array(int fd, int64_t *arr, int n) { int i; assert(fd>1 && "Invalid file descriptor"); for( i=0; i<n; i++ ) { fd_printf(fd, "%" PRId64 "\n", arr[i]); } return 0; }
+
+int write_float_array(int fd, float *arr, int n) { int i; assert(fd>1 && "Invalid file descriptor"); for( i=0; i<n; i++ ) { fd_printf(fd, "%" ".16f" "\n", arr[i]); } return 0; }
+int write_double_array(int fd, double *arr, int n) { int i; assert(fd>1 && "Invalid file descriptor"); for( i=0; i<n; i++ ) { fd_printf(fd, "%" ".16f" "\n", arr[i]); } return 0; }
+
+int write_section_header(int fd) {
+  assert(fd>1 && "Invalid file descriptor");
+  fd_printf(fd, "%%%%\n");
+  return 0;
+}
---
+++ hls_eval_data/machsuite/sort_merge/sort_merge.h
@@ -0,0 +1,18 @@
+#include <stdint.h>
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+
+#define SIZE 2048
+#define TYPE int32_t
+#define TYPE_MAX INT32_MAX
+
+void ms_mergesort(TYPE a[SIZE]);
+
+// Test harness interface code.
+
+struct bench_args_t {
+  TYPE a[SIZE];
+};
